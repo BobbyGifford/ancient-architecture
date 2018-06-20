1c1b4ed42ac8008fd62ddf85ac002c3e363f8c57
Bobby <bobby077joe@gmail.com>
2018-06-20 11:26:28 -0700

    Added Profile schema.

---
+++ models/Profile.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Profile schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 30
+  },
+  fromWhere: {
+    type: String
+  },
+  livingWhere: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  locationsOfInterest: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
