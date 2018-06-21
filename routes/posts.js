e7f4b30182f5b5c85ecf1eba35249e54636a7d0f
Bobby <bobby077joe@gmail.com>
2018-06-20 19:05:44 -0700

    Added POST route to posts router.

--- routes/posts.js
+++ routes/posts.js
@@ -9,12 +9,29 @@ const Post = require("../models/Post");
 // Profile model
 const Profile = require("../models/Profile");
 
-// @route   GET api/posts/test
-// @desc    Tests post route
-// @access  Public
+// @route   POST api/posts
+// @desc    Creates a post
+// @access  Private
 
-router.get("/", requireLogin, (req, res) => {
-  res.send({ message: "post router works with auth also" });
+router.post("/", requireLogin, (req, res) => {
+  const newPost = {};
+
+  newPost.user = req.user.id;
+  if (req.body.title) newPost.title = req.body.title;
+  if (req.body.location) newPost.location = req.body.location;
+  if (req.body.description) newPost.description = req.body.description;
+
+  if (typeof req.body.keyfeatures !== "undefined") {
+    newPost.keyfeatures = req.body.keyfeatures.split(", ");
+  }
+
+  newPost.media = {};
+  if (req.body.youtube) newPost.media.youtube = req.body.youtube;
+  if (req.body.wikipedia) newPost.media.wikipedia = req.body.wikipedia;
+
+  new Post(newPost).save().then(profile => {
+      res.json(profile)
+  })
 });
 
 module.exports = router;
