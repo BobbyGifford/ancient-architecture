6e86d2f8df16ccc106b7abec8f5d9c64d0e666bc
Bobby <bobby077joe@gmail.com>
2018-06-20 18:11:43 -0700

    Created Post schema.

---
+++ models/Post.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  keyfeatures: {
+    type: [String],
+    required: true
+  },
+  description: {
+    type: String
+  },
+  media: {
+    youtube: {
+      type: String
+    },
+    wikipedia: {
+      type: String
+    }
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      date: Date,
+      default: Date.now
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
