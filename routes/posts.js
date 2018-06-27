7cd20917e48bb0beb0d219478ac1ea714b89b6a9
Bobby <bobby077joe@gmail.com>
2018-06-27 15:08:54 -0700

    Added get and delete post by id. Also changed get all post from /all to just /

--- routes/posts.js
+++ routes/posts.js
@@ -38,13 +38,44 @@ router.post("/", requireLogin, (req, res) => {
 // @desc    Fetches all posts
 // @access  Private
 
-router.get('/all', requireLogin, (req, res) => {
-  Post.find().populate("user", ["displayName", "googleImg"]).then((posts) => {
-    if (!posts) {
-      res.status(404).json("No posts found")
-    }
-    res.status(200).json(posts)
-  })
+router.get('/', requireLogin, (req, res) => {
+  Post.find()
+    .populate("user", ["displayName", "googleImg"])
+    .sort({ date: -1 })
+    .then((posts) => {
+      if (!posts) {
+        res.status(404).json("No posts found")
+      }
+      res.status(200).json(posts)
+    })
+})
+
+// @route   GET api/posts/all
+// @desc    Fetches all posts
+// @access  Private
+
+router.get('/:id', requireLogin, (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      res.json(post)
+    })
+    .catch((error) => res.status(404).json({ postnotfound: "No post found" }))
+})
+
+// @route   GET api/posts/all
+// @desc    Fetches all posts
+// @access  Private
+
+router.delete('/:id', requireLogin, (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      // Check for creator of post
+      if (post.user.toString() !== req.user.id) {
+        res.status(401).json({ Unauthorized: "You're not the post's author" })
+      }
+      post.remove().then(() => res.json({ message: "Post was removed" }))
+    })
+    .catch(() => { res.json({ error: "No post found" }) })
 })
 
 module.exports = router;
