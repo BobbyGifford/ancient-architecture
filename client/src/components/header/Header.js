0222e9e02f340737ca4403960e95f4fb9dddd86a
Bobby <bobby077joe@gmail.com>
2018-07-10 11:34:30 -0700

    Added client folder. Front end started.

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+
+const Header = () => (
+        <div>
+            <nav>
+                <div class="nav-wrapper #263238 blue-grey darken-4">
+                    <a href="#" class="brand-logo center">Ancient Architecture</a>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+)
+
+export default Header;
