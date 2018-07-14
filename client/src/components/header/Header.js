5e3e665c7f66d9b5908403d005c873646b1cb4f7
Bobby <bobby077joe@gmail.com>
2018-07-13 17:27:31 -0700

    Unauth home page fixed.

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 class Header extends Component {
     renderContent() {
-        if (this.props.auth === null || this.props.auth === undefined) {
-            return <li className="nav-item" ><a className="nav-link" href="/auth/google">Login</a></li>
+        if (this.props.auth === null || this.props.auth === undefined || this.props.auth === false) {
+            return <a className="btn btn-success" href="/auth/google">Login</a>
         } else {
             return (
-                <ul className="navbar-nav">
+                <ul className="navbar-nav mr-auto">
                     <li className="nav-item"><Link className="nav-link" to="/profile">{this.props.auth.displayName}</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/posts">Posts</Link></li>
                 </ul>
@@ -20,12 +20,16 @@ class Header extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link to="/posts" className="navbar-brand">Ancient Architecture</Link>
+                    {
+                        this.props.auth ? <Link to="/posts" className="navbar-brand">Ancient Architecture</Link> : <Link to="/" className="navbar-brand">Ancient Architecture</Link>
+                    }
+
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         {this.renderContent()}
+                        {this.props.auth ? <a href="/api/logout" className="btn btn-success">Logout</a> : null}
                     </div>
                 </nav>
             </div>
