24d16e8ff89e05bbcee698d4b1da8e5f471ae143
Bobby <bobby077joe@gmail.com>
2018-07-13 19:38:14 -0700

    adfa

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 
 module.exports = app => {
   app.get(
-    "/auth/google",
+    "/api/auth/google",
     passport.authenticate("google", {
       scope: ["profile", "email"],
       prompt: "select_account"
