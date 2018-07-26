c5fea44b668cb8a3d56f9b58950aeb523a4c109f
Bobby <bobby077joe@gmail.com>
2018-07-26 14:45:28 -0700

    Comment routing fixed and other profile looks good.

---
+++ client/src/api/apiKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  key: '6061772-854d79ab54c4b17c47a57fedf',
+};
---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb://bobbydev:bobbydev1@ds263500.mlab.com:63500/ancient-architecture",
+    googleClientID: "16334981290-r3a8sqig2k8pk1ccio6s3qhvkd341sg1.apps.googleusercontent.com",
+    googleClientSecret: "hwE89wdoJQIVaSnxxt2ajvno",
+    cookieKey: "7777777"
+
+}
