07728a8e45e75aca97965e8ac6a27d3d4527c4a5
Bobby <bobby077joe@gmail.com>
2018-09-06 17:58:04 -0700

    Added toggle gallery for posts.

--- client/src/components/posts/pixabayGallery/PixabayGallery.js
+++ client/src/components/posts/pixabayGallery/PixabayGallery.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+import './PixabayGallery.css';
 import api from '../../../api/apiKey';
 
 class PixabayGallery extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      showSingleImg: true,
+    };
+
+    this.handleToggle = this.handleToggle.bind(this);
   }
 
   async componentDidMount() {
@@ -24,22 +30,70 @@ class PixabayGallery extends Component {
   }
 
   renderCards() {
-    return this.state.formattedData.map(pic => {
+    return (
+      <div>
+        {this.state.formattedData.map(pic => {
+          return (
+            <img
+              style={{ maxHeight: '20rem' }}
+              key={pic.largeImageURL}
+              src={pic.largeImageURL}
+              alt={pic.largeImageURL}
+            />
+          );
+        })}
+        <div>
+          <button className="btn btn-show btn-info" onClick={this.handleToggle}>
+            Toggle gallery
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  renderSingleCard() {
+    if (this.state.formattedData[0] !== undefined) {
       return (
-        <img
-          style={{ maxHeight: '20rem' }}
-          key={pic.largeImageURL}
-          src={pic.largeImageURL}
-          alt={pic.largeImageURL}
-        />
+        <div>
+          <img
+            className="single_feature"
+            src={this.state.formattedData[0].largeImageURL}
+            alt="pic"
+          />
+          <button className="btn btn-show btn-info" onClick={this.handleToggle}>
+            Toggle gallery
+          </button>
+        </div>
       );
-    });
+    } else {
+      return (
+        <div className="col offset-6">
+          <h1>Pixabay Returned 0 results</h1>
+        </div>
+      );
+    }
+  }
+
+  handleToggle() {
+    console.log('toggle');
+    this.setState({ showSingleImg: !this.state.showSingleImg });
   }
 
   render() {
     return (
       <div className="row">
-        {this.state.formattedData ? this.renderCards() : <h1>Loading</h1>}
+        {this.state.formattedData !== undefined ? (
+          <div className="text-center">
+            {this.state.showSingleImg ? (
+              <div className="text-center">{this.renderSingleCard()}</div>
+            ) : (
+              <div className="text-center">{this.renderCards()}</div>
+            )}
+            <div />
+          </div>
+        ) : (
+          <h1>Loading</h1>
+        )}
       </div>
     );
   }
